51b55cf64dff998810bba3c2361478f0a740fb2b
direwolf20 <39863894+Direwolf20-MC@users.noreply.github.com>
2020-09-28 19:34:01 -0400

    Item Flow particles working pretty well!

--- src/main/java/com/direwolf20/logisticslasers/client/particles/itemparticle/ItemFlowParticle.java
+++ src/main/java/com/direwolf20/logisticslasers/client/particles/itemparticle/ItemFlowParticle.java
@@ -4,19 +4,31 @@ import net.minecraft.client.particle.BreakingParticle;
 import net.minecraft.client.particle.IParticleFactory;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.vector.Vector3d;
 
 public class ItemFlowParticle extends BreakingParticle {
 
-    public ItemFlowParticle(ClientWorld world, double x, double y, double z, double xSpeed, double ySpeed, double zSpeed, ItemStack itemStack) {
+    private double targetX, targetY, targetZ;
+
+    public ItemFlowParticle(ClientWorld world, double x, double y, double z, double targetX, double targetY, double targetZ, ItemStack itemStack, int ticksPerBlock) {
         this(world, x, y, z, itemStack);
         this.motionX = 0;
         this.motionY = 0;
         this.motionZ = 0;
-        this.motionX += xSpeed;
-        this.motionY += ySpeed;
-        this.motionZ += zSpeed;
+        this.targetX = targetX;
+        this.targetY = targetX;
+        this.targetZ = targetX;
+        Vector3d target = new Vector3d(targetX, targetY, targetZ);
+        Vector3d source = new Vector3d(this.posX, this.posY, this.posZ);
+        Vector3d path = target.subtract(source).normalize().mul(1, 1, 1);
+        this.motionX += path.x / ticksPerBlock;
+        this.motionY += path.y / ticksPerBlock;
+        this.motionZ += path.z / ticksPerBlock;
         this.particleGravity = 0.0f;
-        this.maxAge = 40;
+        double distance = target.distanceTo(source);
+        this.maxAge = (int) distance * ticksPerBlock;
+        //System.out.println(path + ":" + distance + ":" + maxAge);
+        this.canCollide = false;
     }
 
     public ItemFlowParticle(ClientWorld world, double x, double y, double z, ItemStack itemStack) {
@@ -25,11 +37,18 @@ public class ItemFlowParticle extends BreakingParticle {
 
     @Override
     public void tick() {
-        //System.out.println("I'm Alive");
-        super.tick();
+        this.prevPosX = this.posX;
+        this.prevPosY = this.posY;
+        this.prevPosZ = this.posZ;
+        if (this.age++ >= this.maxAge) {
+            this.setExpired();
+        } else {
+            this.motionY -= 0.04D * (double) this.particleGravity;
+            this.move(this.motionX, this.motionY, this.motionZ);
+        }
     }
 
     public static IParticleFactory<ItemFlowParticleData> FACTORY =
             (data, world, x, y, z, xSpeed, ySpeed, zSpeed) ->
-                    new ItemFlowParticle(world, x, y, z, xSpeed, ySpeed, zSpeed, data.getItemStack());
+                    new ItemFlowParticle(world, x, y, z, data.targetX, data.targetY, data.targetZ, data.getItemStack(), data.ticksPerBlock);
 }
--- src/main/java/com/direwolf20/logisticslasers/client/particles/itemparticle/ItemFlowParticleData.java
+++ src/main/java/com/direwolf20/logisticslasers/client/particles/itemparticle/ItemFlowParticleData.java
@@ -3,7 +3,6 @@ package com.direwolf20.logisticslasers.client.particles.itemparticle;
 import com.direwolf20.logisticslasers.client.particles.ModParticles;
 import com.mojang.brigadier.StringReader;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
-import com.mojang.serialization.Codec;
 import net.minecraft.command.arguments.ItemInput;
 import net.minecraft.command.arguments.ItemParser;
 import net.minecraft.item.ItemStack;
@@ -18,18 +17,26 @@ import javax.annotation.Nonnull;
 
 public class ItemFlowParticleData implements IParticleData {
     private final ItemStack itemStack;
+    public final double targetX;
+    public final double targetY;
+    public final double targetZ;
+    public final int ticksPerBlock;
 
-    public ItemFlowParticleData(ItemStack itemStack) {
+    public ItemFlowParticleData(ItemStack itemStack, double tx, double ty, double tz, int ticks) {
         this.itemStack = itemStack.copy(); //Forge: Fix stack updating after the fact causing particle changes.
+        targetX = tx;
+        targetY = ty;
+        targetZ = tz;
+        ticksPerBlock = ticks;
     }
 
-    public static Codec<ItemFlowParticleData> func_239809_a_(ParticleType<ItemFlowParticleData> p_239809_0_) {
-        return ItemStack.ITEMSTACK_CODEC.xmap((p_239810_1_) -> {
-            return new ItemFlowParticleData(p_239810_1_);
+    /*public static Codec<ItemFlowParticleData> func_239809_a_(ParticleType<ItemFlowParticleData> p_239809_0_) {
+        return ItemStack.ITEMSTACK_CODEC.xmap((itemStack) -> {
+            return new ItemFlowParticleData(itemStack);
         }, (p_239808_0_) -> {
             return p_239808_0_.itemStack;
         });
-    }
+    }*/
 
     @Nonnull
     @Override
@@ -57,12 +64,20 @@ public class ItemFlowParticleData implements IParticleData {
             reader.expect(' ');
             ItemParser itemparser = (new ItemParser(reader, false)).parse();
             ItemStack itemstack = (new ItemInput(itemparser.getItem(), itemparser.getNbt())).createStack(1, false);
-            return new ItemFlowParticleData(itemstack);
+            reader.expect(' ');
+            double tx = reader.readDouble();
+            reader.expect(' ');
+            double ty = reader.readDouble();
+            reader.expect(' ');
+            double tz = reader.readDouble();
+            reader.expect(' ');
+            int ticks = reader.readInt();
+            return new ItemFlowParticleData(itemstack, tx, ty, tz, ticks);
         }
 
         @Override
         public ItemFlowParticleData read(ParticleType<ItemFlowParticleData> particleTypeIn, PacketBuffer buffer) {
-            return new ItemFlowParticleData(buffer.readItemStack());
+            return new ItemFlowParticleData(buffer.readItemStack(), buffer.readDouble(), buffer.readDouble(), buffer.readDouble(), buffer.readInt());
         }
     };
 }
