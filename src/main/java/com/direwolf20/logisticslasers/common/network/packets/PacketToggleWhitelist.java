7e9b0dfeaad714051a63e49635bf548fc8205496
direwolf20 <39863894+Direwolf20-MC@users.noreply.github.com>
2020-10-26 10:42:36 -0400

    Cleanup whitelist packet

--- src/main/java/com/direwolf20/logisticslasers/common/network/packets/PacketToggleWhitelist.java
+++ src/main/java/com/direwolf20/logisticslasers/common/network/packets/PacketToggleWhitelist.java
@@ -1,10 +1,10 @@
 package com.direwolf20.logisticslasers.common.network.packets;
 
+import com.direwolf20.logisticslasers.common.container.cards.BasicFilterContainer;
 import com.direwolf20.logisticslasers.common.container.cards.TagFilterContainer;
 import com.direwolf20.logisticslasers.common.items.logiccards.BaseCard;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.inventory.container.Container;
-import net.minecraft.inventory.container.Slot;
 import net.minecraft.item.ItemStack;
 import net.minecraft.network.PacketBuffer;
 import net.minecraftforge.fml.network.NetworkEvent;
@@ -12,22 +12,15 @@ import net.minecraftforge.fml.network.NetworkEvent;
 import java.util.function.Supplier;
 
 public class PacketToggleWhitelist {
-    private int slotNumber;
 
     public PacketToggleWhitelist() {
-        this.slotNumber = -1;
-    }
-
-    public PacketToggleWhitelist(int slot) {
-        this.slotNumber = slot;
     }
 
     public static void encode(PacketToggleWhitelist msg, PacketBuffer buffer) {
-        buffer.writeInt(msg.slotNumber);
     }
 
     public static PacketToggleWhitelist decode(PacketBuffer buffer) {
-        return new PacketToggleWhitelist(buffer.readInt());
+        return new PacketToggleWhitelist();
     }
 
     public static class Handler {
@@ -38,20 +31,13 @@ public class PacketToggleWhitelist {
                     return;
 
                 Container container = sender.openContainer;
-
+                if (container == null) return;
                 ItemStack itemStack;
 
                 if (container instanceof TagFilterContainer) {
                     itemStack = ((TagFilterContainer) container).filterItemStack;
                 } else {
-                    if (msg.slotNumber == -1) {
-                        itemStack = sender.getHeldItemMainhand();
-                        if (!(itemStack.getItem() instanceof BaseCard))
-                            itemStack = sender.getHeldItemOffhand();
-                    } else {
-                        Slot slot = container.inventorySlots.get(msg.slotNumber);
-                        itemStack = slot.getStack();
-                    }
+                    itemStack = ((BasicFilterContainer) container).filterItemStack;
                 }
                 if (itemStack.getItem() instanceof BaseCard) {
                     BaseCard.setWhiteList(itemStack, !BaseCard.getWhiteList(itemStack));
