a282e0d466d91489fe06dbcf7e392bb0abf8351f
direwolf20 <39863894+Direwolf20-MC@users.noreply.github.com>
2020-11-02 13:04:22 -0500

    JEI Shift Click in Crafting Window now gets the ingredient we have most of.

--- src/main/java/com/direwolf20/logisticslasers/client/jei/CraftingStationRecipeTransferHandler.java
+++ src/main/java/com/direwolf20/logisticslasers/client/jei/CraftingStationRecipeTransferHandler.java
@@ -3,6 +3,7 @@ package com.direwolf20.logisticslasers.client.jei;
 import com.direwolf20.logisticslasers.common.container.CraftingStationContainer;
 import com.direwolf20.logisticslasers.common.network.PacketHandler;
 import com.direwolf20.logisticslasers.common.network.packets.PacketFilterSlot;
+import com.direwolf20.logisticslasers.common.util.ItemHandlerUtil;
 import mezz.jei.api.gui.IRecipeLayout;
 import mezz.jei.api.gui.ingredient.IGuiIngredient;
 import mezz.jei.api.recipe.transfer.IRecipeTransferError;
@@ -25,14 +26,29 @@ public class CraftingStationRecipeTransferHandler implements IRecipeTransferHand
     public IRecipeTransferError transferRecipe(CraftingStationContainer container, Object recipe, IRecipeLayout recipeLayout, PlayerEntity player, boolean maxTransfer, boolean doTransfer) {
         if (doTransfer) {
             Map<Integer, ? extends IGuiIngredient<ItemStack>> guiIngredients = recipeLayout.getItemStacks().getGuiIngredients();
-            
+            boolean hasController = container.tile.hasController();
+            ItemHandlerUtil.InventoryCounts inventoryCounts = new ItemHandlerUtil.InventoryCounts();
+            if (hasController)
+                inventoryCounts = container.tile.getControllerTE().getItemCounts();
             for (Map.Entry<Integer, ? extends IGuiIngredient<ItemStack>> entry : guiIngredients.entrySet()) {
                 int recipeSlot = entry.getKey();
                 List<ItemStack> allIngredients = entry.getValue().getAllIngredients();
                 if (!allIngredients.isEmpty()) {
                     if (recipeSlot != 0) { // skip the output slot
-                        ItemStack firstIngredient = allIngredients.get(0);
-                        PacketHandler.sendToServer(new PacketFilterSlot(26 + recipeSlot, firstIngredient, firstIngredient.getCount()));
+                        if (hasController) {
+                            int amt = 0;
+                            ItemStack mostStack = allIngredients.get(0);
+                            for (ItemStack stack : allIngredients) {
+                                if (inventoryCounts.getCount(stack) > amt) {
+                                    amt = inventoryCounts.getCount(stack);
+                                    mostStack = stack.copy();
+                                }
+                            }
+                            PacketHandler.sendToServer(new PacketFilterSlot(26 + recipeSlot, mostStack, mostStack.getCount()));
+                        } else {
+                            ItemStack firstIngredient = allIngredients.get(0);
+                            PacketHandler.sendToServer(new PacketFilterSlot(26 + recipeSlot, firstIngredient, firstIngredient.getCount()));
+                        }
                     }
                 } else { //If theres no recipe ingredient here, clear the slot
                     PacketHandler.sendToServer(new PacketFilterSlot(26 + recipeSlot, ItemStack.EMPTY, 0));
